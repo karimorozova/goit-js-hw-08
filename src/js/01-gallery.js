72b7f9bac0eca436aff7f0973df1e1e0e4a7f43b
karine-morozova <morozova_karine@ukr.net>
2022-02-01 22:25:36 +0200

    Task 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,43 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+console.log(galleryRef);
+
+
+
+
+renderGallery(galleryItems);
+
+
+let gallery = new SimpleLightbox('.gallery__item');
+    gallery.on('show.simplelightbox');
+
+
+galleryRef.addEventListener('click', (event) => event.preventDefault());
+
+
+function renderGallery(arr) {
+    const galleryStr = arr.map(img => `
+    
+  <a class="gallery__item" href="${img.original}">
+    <img
+      class="gallery__image"
+      src="${img.preview}"
+      
+      alt="${img.description}"
+      title="${img.description}"
+    />
+  </a>
+    `).join('');
+
+    galleryRef.insertAdjacentHTML('beforeend', galleryStr );
+}
+
+   
