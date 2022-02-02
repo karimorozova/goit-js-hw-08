0d9a4845efa25a69cb350ceb8565b74b8b75c653
karine-morozova <morozova_karine@ukr.net>
2022-02-02 18:45:09 +0200

    Add task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,24 +1,42 @@
+const throttle = require('lodash.throttle');
+
 const formRef = document.querySelector('.feedback-form');
-console.log(formRef);
+// console.log(formRef);
 const FORM_STATE_STORAGE_KEY = "feedback-form-state";
 let formData = {};
+const { elements: { email, message } } = formRef;
 
 
-formRef.addEventListener('input', onFormInput);
+fillInputsOfFeedbackForm();
+
+formRef.addEventListener('input', throttle(onFormInput, 500));
 formRef.addEventListener('submit', onFormSubmit);
 
 function onFormInput(event) {
-// console.log(event.target.name)
+
 formData[event.target.name] = event.target.value;
-// console.log(formData);
+
 localStorage.setItem(FORM_STATE_STORAGE_KEY, JSON.stringify(formData))
+
 };
 
 function onFormSubmit(event) {
     event.preventDefault();
     event.currentTarget.reset();
-    localStorage.clear()
+    localStorage.removeItem(FORM_STATE_STORAGE_KEY);
+    console.log(formData);
 }
+
+function fillInputsOfFeedbackForm() {
+    const savedData = JSON.parse(localStorage.getItem(FORM_STATE_STORAGE_KEY));
+
+    if (savedData) {
+        
+      email.value = savedData.email ?? "";
+      message.value = savedData.message ?? "";
+    }
+
+  }
     
 
 
