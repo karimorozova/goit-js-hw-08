c2adb2ea9f41498bd599a89198d28f7d86c7a865
karine-morozova <morozova_karine@ukr.net>
2022-02-02 18:03:39 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,25 @@
+const formRef = document.querySelector('.feedback-form');
+console.log(formRef);
+const FORM_STATE_STORAGE_KEY = "feedback-form-state";
+let formData = {};
+
+
+formRef.addEventListener('input', onFormInput);
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormInput(event) {
+// console.log(event.target.name)
+formData[event.target.name] = event.target.value;
+// console.log(formData);
+localStorage.setItem(FORM_STATE_STORAGE_KEY, JSON.stringify(formData))
+};
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+    localStorage.clear()
+}
+    
+
+
 
